11a2ae91801baa9191c5506d5300b42e3a65d3b2
Daniel <danielghirmayh@gmail.com>
2024-06-19 22:36:04 -0400

    Update App.js
    This is the routes with the links

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from'./Header';
+import Cart from'./Cart';
+import Home from'./Home';
+import Topnavbar from './Topnavbar';
+import Bottomnavbar from './Bottomnavbar';
+import Signin from'./Signin';
+import Topfooter from'./Topfooter';
+import Languagefooter from'./Languagefooter';
+import Bottomfooter from'./Bottomfooter';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+   <div className="App">
+   <Header/>
+   <Topnavbar/>
+   <Bottomnavbar/>
+    <Routes>
+    <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+
+
+    </Routes>
+    <Signin/>
+   <Topfooter/>
+   <Languagefooter/>
+   <Bottomfooter/>
+      
     </div>
+    </Router>
   );
 }
 
