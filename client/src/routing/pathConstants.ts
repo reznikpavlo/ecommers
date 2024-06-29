328a28993da6600256fb6fc0cb0f6eb5f8a141e1
Zakharii-Husar <zakhariihusar@gmail.com>
2024-06-29 14:46:39 -0500

    Created client structure with templates. Installed typescrip and react router

---
+++ client/src/routing/pathConstants.ts
@@ -0,0 +1,12 @@
+
+const PATH = {
+    index: "/",
+    chats: "/chats",
+    currentChat: "/chats/:chatId",
+    users: "/users",
+    userProfile: "/users/:userName",
+    login: "/login",
+    register: "/register",
+}
+
+export default PATH;
