29411e48e27db67ac91a2e89291f1a9d0e5a8aba
Zakharii-Husar <zakhariihusar@gmail.com>
2024-07-11 02:00:38 -0500

    DB init code

--- api/main.go
+++ api/main.go
@@ -1,35 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/jackc/pgx"
+	"api/data"
 )
 
 func main() {
-    // Define the connection configuration
-    connConfig := pgx.ConnConfig{
-        Host:     "localhost",
-        Port:     5432,
-        Database: "amazon",
-        User:     "amazon_user",
-        Password: "securepassword",
-    }
-
-    // Establish a PostgreSQL connection
-    conn, err := pgx.Connect(connConfig)
-    if err != nil {
-        log.Fatalf("Unable to connect to database: %v\n", err)
-    }
-    defer conn.Close()
-
-    // Perform a query
-    var greeting string
-    err = conn.QueryRow("SELECT 'Hello, world!'").Scan(&greeting)
-    if err != nil {
-        log.Fatalf("QueryRow failed: %v\n", err)
-    }
-
-    fmt.Println(greeting)
+	data.Init()
 }
