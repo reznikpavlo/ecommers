a82c02dbf6337a0095fb8533ab0383d4b1fcd753
Zakharii-Husar <zakhariihusar@gmail.com>
2024-07-25 20:17:15 -0500

    Added mock product details for testing ProductDetailsInterface

--- client/src/components/products/ProductDetails.tsx
+++ client/src/components/products/ProductDetails.tsx
@@ -1,36 +1,40 @@
 import styled from 'styled-components'
 import { useState, useEffect } from 'react';
 import { GET_PRODUCT_BY_ID } from '../../routing/APIEndpoints';
+import { productDetailsMock } from './ProductDetailsMock';
 
 
 import type { paths, components } from "../../types/types";
 //⭐⭐⭐⭐⭐
 
-type ProductDetailsType = components["schemas"]["ProductDetailsResponse"];
-type EndpointParams = paths["/api/products/{product_id}"]["parameters"];
-
-const [productDetails, setProductDetails] = useState<null | ProductDetailsType>(null);
-
-const fetchProductDetails = async () => {
-  const url = GET_PRODUCT_BY_ID("product_id");
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-    setProductDetails(json);
-  } catch (error) {
-    console.error("Couldn't load product!");
-  }
-}
-
-useEffect(()=>{
-  if(productDetails === null) fetchProductDetails();
-}, [productDetails, fetchProductDetails]);
-
 function Product() {
+  type ProductDetailsType = components["schemas"]["ProductDetailsResponse"];
+  type EndpointParams = paths["/api/products/{product_id}"]["parameters"];
+
+
+  const [productDetails, setProductDetails] = useState<null | ProductDetailsType>(null);
+
+  const fetchProductDetails = async () => {
+    const url = GET_PRODUCT_BY_ID("product_id");
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      setProductDetails(json);
+    } catch (error) {
+      setProductDetails(productDetailsMock);
+      console.error("Couldn't load product!");
+    }
+  }
+
+  useEffect(() => {
+    if (productDetails === null) fetchProductDetails();
+    console.log(productDetails);
+  }, [productDetails, fetchProductDetails]);
+
   return (
     <Container>
       <AttachmentsContainer>Attachments</AttachmentsContainer>
---
+++ client/src/components/products/ProductDetailsMock.tsx
@@ -0,0 +1,57 @@
+export const productDetailsMock = {
+    id: "prod_id",
+    category: "Electronics",
+    attachments: [{ image: "link" }],
+    delivery: {
+        handlingTime: "2 days",
+        shippingRates: {
+            id: "shipping_rate_id"
+        },
+        additionalInfo: "Shipping processed MON to FRI",
+        /** Format: date-time */
+        shipmentDate: "08/08/2024 03:05:15 PM"
+    },
+    publication: {
+        duration: "2 months",
+        /** Format: date-time */
+        endAt: "08/08/2024 03:05:15 PM",
+        /** Format: date-time */
+        startAt: "08/08/2024 03:05:15 PM",
+        status: "Active",
+        republish: false,
+        endedBy: ""
+    },
+    language: "EN",
+    validation: {
+        error: [], //array of strings
+        warnings: [], //array of strings
+        /** Format: date-time */
+        validationDate: "08/08/2024 03:05:15 PM"
+    },
+    saleAfterService: {
+        warranty: {
+            id: "warranty_id"
+        },
+        returnPolicy: {
+            id: "return_policy_id"
+        },
+        userWarranty: {
+            id: "user_warranty_id"
+        }
+    },
+    stock: {
+        avaiable: 21,
+        unit: "stuck"
+    },
+    contact: {
+        id: "contact_id"
+    },
+    locations: {
+        id: "location_id",
+        city: "Edmonton",
+        country: "CA",
+        postCode: "R21 3U7",
+        province: "Alberta"
+    },
+    tax: { rates: { rate: 15, country: "CA", province: "Alberta" } }
+};
