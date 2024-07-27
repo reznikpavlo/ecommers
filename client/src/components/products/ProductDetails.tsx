043fc0535e6a5f6529025c6093f33630b0c18ef1
Zakharii-Husar <zakhariihusar@gmail.com>
2024-07-27 14:45:42 -0500

    Working on product details interface icons styles

--- client/src/components/products/ProductDetails.tsx
+++ client/src/components/products/ProductDetails.tsx
@@ -3,37 +3,39 @@ import { useState, useEffect } from 'react';
 import { GET_PRODUCT_BY_ID } from '../../routing/APIEndpoints';
 import { productDetailsMock } from './ProductDetailsMock';
 
-
-import type { paths, components } from "../../types/types";
+//import type { paths, components } from "../../types/types";
+import type { components } from "../../types/types";
 //⭐⭐⭐⭐⭐
 
 function Product() {
   type ProductDetailsType = components["schemas"]["ProductDetailsResponse"];
-  type EndpointParams = paths["/api/products/{product_id}"]["parameters"];
+  //type EndpointParams = paths["/api/products/{product_id}"]["parameters"];
 
 
   const [productDetails, setProductDetails] = useState<null | ProductDetailsType>(null);
 
-  const fetchProductDetails = async () => {
-    const url = GET_PRODUCT_BY_ID("product_id");
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-
-      const json = await response.json();
-      setProductDetails(json);
-    } catch (error) {
-      setProductDetails(productDetailsMock);
-      console.error("Couldn't load product!");
-    }
-  }
 
   useEffect(() => {
+
+    const fetchProductDetails = async () => {
+      const url = GET_PRODUCT_BY_ID("product_id");
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        setProductDetails(json);
+      } catch (error) {
+        setProductDetails(productDetailsMock);
+        console.error("Couldn't load product!");
+      }
+    }
+
     if (productDetails === null) fetchProductDetails();
     console.log(productDetails);
-  }, [productDetails, fetchProductDetails]);
+  }, [productDetails]);
 
   return (
     <Container>
@@ -46,7 +48,7 @@ function Product() {
           }
         </IconsContainer>
         <MainImgContainer>
-          <MainImg src={productDetails?.attachments[0]}/>
+          <MainImg src={productDetails?.attachments[0]} />
         </MainImgContainer>
       </AttachmentsContainer>
       <InfoContainer>Info</InfoContainer>
